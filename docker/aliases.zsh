b1a825ea92179b0c4affcd114a11444e896dbf2e
Lukas Ramm <lukas.ramm.1998@gmail.com>
2019-05-20 21:14:23 +0200

    edit aliase cause of new functions

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -7,15 +7,12 @@ if [[ $OSTYPE == darwin* ]]; then
   alias moby='docker run --rm -it --privileged --pid=host walkerlee/nsenter -t 1 -m -u -i -n sh'
 fi
 
+# Funktionen stehen im Ordner docker/bin
+
 # Status anzeige
-alias dps='docker ps'
 alias dcl='docker container ls'
 
 # Standard Befehle
 alias dpull='docker pull'
 alias dpush='docker push'
 alias db='docker build'
-
-# Docker-Compose
-alias dup='docker-compose up'
-alias ddown='docker-compose down'
