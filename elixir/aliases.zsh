a7fb83d25fc9956deaafb3aacd688277ab398c09
Lukas Ramm <lukas.ramm.1998@gmail.com>
2019-11-01 14:11:55 +0100

    remove elixir

--- elixir/aliases.zsh
+++
@@ -1,7 +0,0 @@
-(($+commands[elixir])) || return 0
-[[ "$(platform)" == 'windows' ]] || return 0
-
-verbose Setting up Windows Elixir aliases
-
-# Initial Elixir aliases, call again if you installed new binaries.
-update-elixir-aliases --no-verbose
