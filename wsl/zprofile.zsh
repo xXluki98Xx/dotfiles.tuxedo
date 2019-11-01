17cf2f5bc29ea0c2881509d8a54c0a42778d28a7
Lukas Ramm <lukas.ramm.1998@gmail.com>
2019-11-01 14:15:06 +0100

    remove wsl

--- wsl/zprofile.zsh
+++
@@ -1,9 +0,0 @@
-if [[ -f /proc/version ]] && \
-   grep --quiet Microsoft /proc/version; then
-  [[ "$(umask)" == '000' ]] && umask 022
-
-  # https://github.com/Microsoft/BashOnWindows/issues/1887
-  unsetopt BG_NICE
-
-  sudo mount --all
-fi
